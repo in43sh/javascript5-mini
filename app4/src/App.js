a725e72ac431ffc6a6b92ad1b7f082d0b4290b82
Sergey Sherstobitov <in43sh@gmail.com>
2018-01-09 14:50:07 -0700

    app4 done

--- app4/src/App.js
+++ app4/src/App.js
@@ -8,22 +8,39 @@ class App extends Component {
     this.state = {
       cars: []
     }
+    this.getCars = this.getCars.bind(this)
   }
 
   getCars() {
     axios.get('https://joes-autos.herokuapp.com/api/vehicles')
     .then(res => {
       this.setState({
-        cars: res
+        cars: res.data
       })
+    })
+    .catch(err => {
+      console.log(err)
     })
   }
 
   render() {
+    const displayCars = this.state.cars.map((car, i) => {
+      return (
+        <div>
+          <p>{car.make}</p>
+          <p>{car.model}</p>
+          <p>{car.color}</p>
+          <p>{car.price}</p>
+          <p>{car.year}</p>
+        </div>
+      )
+    })
+
     return (
       <div className="App">
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+        {/* {this.state.cars} */}
+        { displayCars }
       </div>
     );
   }
